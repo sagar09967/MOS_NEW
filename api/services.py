6b87e4c277f7dc98548ab9271210f3c353247df6
devasyadave <devasyadave@gmail.com>
2022-12-10 11:59:31 +0530

    ssl verify false

--- api/services.py
+++ api/services.py
@@ -2,7 +2,7 @@ import json, requests
 
 
 def get_market_rate(part):
-    response = requests.post('https://mosapi.sinewave.co.in/stocks/', json={'tickers': part})
+    response = requests.post('https://mosapi.sinewave.co.in/stocks/', json={'tickers': part},verify=False)
     string = response.json()
     json_res = json.loads(string)
     if len(json_res) > 0:
