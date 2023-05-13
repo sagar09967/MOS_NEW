ad72a6724c9738906161ce30fb4ad5bd77a4f8f5
devasyadave <devasyadave@gmail.com>
2023-05-13 22:00:24 +0530

    Stock Exchange APIs

--- api/stock_exchange_urls.py
+++ api/stock_exchange_urls.py
@@ -1,8 +1,8 @@
 from . import views, post_feedback_release_views as pfr_views
 from rest_framework.routers import DefaultRouter
 from django.urls import path, re_path
-from .stock_exchange_views import get_list_of_indexes,get_symbol_list_by_index
+from .stock_exchange_views import get_list_of_exchanges,get_symbol_list_by_exchange
 urlpatterns = [
-    path("indexes",get_list_of_indexes),
-    path("symbols",get_symbol_list_by_index)
+    path("stock_exchanges",get_list_of_exchanges),
+    path("stock_symbols",get_symbol_list_by_exchange)
 ]
--- api/stock_exchange_views.py
+++ api/stock_exchange_views.py
@@ -1,11 +1,13 @@
 from rest_framework import request
 from rest_framework.response import Response
-from stocksymbol import StockSymbol
+# from stocksymbol import StockSymbol
 from rest_framework.decorators import api_view
-from .stock_exchange_models import Index
+# from .stock_exchange_models import Index
+# from yahoo_fin import stock_info as si
+# import json
+# api_key = '0aeab093-8852-4021-940e-f063fb69d08b'
+# ss = StockSymbol(api_key)
 
-api_key = '0aeab093-8852-4021-940e-f063fb69d08b'
-ss = StockSymbol(api_key)
 
 # get symbol list based on market
 # symbol_list_us = ss.get_symbol_list(market="IN")  # "us" or "america" will also work
@@ -20,15 +22,29 @@ ss = StockSymbol(api_key)
 # index_list = ss.index_list
 #
 # default_obj = None
+from api.stock_exchange_models import StockExchange, StockSymbol
 
 
 @api_view(['GET'])
-def get_list_of_indexes(request):
-    indexes = list(Index.objects.filter(is_active=True).values())
-    return Response({"status": True, "data": indexes})
-
+def get_list_of_exchanges(request):
+    exchanges = list(StockExchange.objects.filter(is_active=True).values())
+    return Response({"status": True, "data": exchanges})
 
 @api_view(['GET'])
-def get_symbol_list_by_index(request):
-    index = request.query_params.get("index")
-    return Response({"status": True, "data": ss.get_symbol_list(index=index)})
+def get_symbol_list_by_exchange(request):
+    stock_exchange = request.query_params.get("stock_exchange")
+    symbols = list(StockSymbol.objects.filter(stock_exchange=stock_exchange).values())
+    return Response({"status": True, "data": symbols})
+
+# @api_view(['GET'])
+# def get_list_of_indexes(request):
+#     indexes = list(Index.objects.filter(is_active=True).values())
+#     return Response({"status": True, "data": indexes})
+#
+#
+# @api_view(['GET'])
+# def get_symbol_list_by_index(request):
+#     #companies = si.tickers_other(request.query_params.get("index"))
+#     index = request.query_params.get("index")
+#     tickers = ss.get_symbol_list(index=index)
+#     return Response({"status": True, "data": tickers})
